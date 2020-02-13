61d9b33f930c5f522d31e71e119031f81ff139e2
Jaxel Rojas <jaxelr@gmail.com>
2020-02-13 11:43:23 -0400

    added vue-persisted-store sample

---
+++ vue-persistent-store/README.md
@@ -0,0 +1,3 @@
+# Vue-Persistent-Store
+
+Tutorial taken from [this article](https://dev.to/nickitax/persistent-store-with-vuejs-vuex-and-vuex-persisted-state-354n)
---
+++ vue-persistent-store/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>The user logged in is: {{this.userId}}</div>
+    <button @click="login()">Login as Jaxel</button>
+    <button @click="logout()">Logout</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'App',
+  computed: mapState({
+    userId: state => state.account.userId
+  }),
+  methods: {
+    login() {
+      this.$store.commit('login', 'Jaxel')
+    },
+    logout() {
+      this.$store.commit('logout')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
